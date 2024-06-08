bc30f336cc811240460b4b86a60160a33171b62e
sherif-abouzaid-tt <152188935+sherif-abouzaid-tt@users.noreply.github.com>
2024-06-08 20:16:25 +0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/.',
+  base: './',
   plugins: [
     vue({
       template: {
@@ -21,7 +21,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue-i18n': 'vue-i18n/dist/vue-i18n.esm-bundler.js'
+      'vue-i18n': 'vue-i18n/build/vue-i18n.esm-bundler.js'
     }
   },
   css: {
