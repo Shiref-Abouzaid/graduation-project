099829b4ffc188b9654a630f679cfb60f1dadcf9
sherif-abouzaid-tt <152188935+sherif-abouzaid-tt@users.noreply.github.com>
2024-06-08 19:14:45 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/graduation-project/',
+  base: './',
   plugins: [
     vue({
       template: {
