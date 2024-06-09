1bc404b8d998dda22ad8a2c4a747fe5d60c172d3
sherif-abouzaid-tt <152188935+sherif-abouzaid-tt@users.noreply.github.com>
2024-06-09 03:00:53 +0300

    Fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createRouter, createWebHashHistory} from 'vue-router';
+import { createRouter, createWebHistory,  } from 'vue-router';
 import MainRoutes from './MainRoutes';
 import AuthRoutes from './AuthRoutes';
 import { useAuthStore } from '@/stores/auth';
 import { useUIStore } from '@/stores/ui';
 
 export const router = createRouter({
-  history: createWebHashHistory (),
+  history: createWebHistory(import.meta.env.BASE_URL),
 
   routes: [
     {
